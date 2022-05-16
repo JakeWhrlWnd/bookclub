423704a38e6348527888de88d954de348817f283
jakeWHRLWND <justjamescreative@outlook.com>
2022-05-15 22:27:16 -0400

    Added to the js

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 let myLibrary = [];
 
-function Book() {
+const bookTitle = document.getElementById('#title');
+const bookAuthor = document.getElementById('#author');
 
+class Book {
+    constructor(title, author) {
+        this.title = bookTitle;
+        this.author = bookAuthor;
+    }
 }
 
 function addBookToLibrary() {
-    
+
 }
+
+
+/*
+User inputs Title and Author of book
+User presses the ADD Button
+When ADD Button is pressed a book entry is created
+
+div - book entry
+    div - (p) title, div - (p) author, div - (btn) status, div - (btn) remove
+
+
+*/
+
+/*
+function getTitle() {
+    return bookTitle;
+}
+function getAuthor() {
+    return bookArthor;
+}
+
+function getReadStatus() {
+
+}
+addBookToLibrary()
+*/  
